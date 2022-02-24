94b5824a09c5f6224efbea031519d93c6828d4df
yeosiowvic <yeosiowvic@gmail.com>
2022-02-24 15:30:59 +0800

    add widthPercent

--- src/utils/dimensions.js
+++ src/utils/dimensions.js
@@ -3,15 +3,21 @@ import { Dimensions } from 'react-native';
 const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
 
 /**
- * @name widthPixcel
+ * @name widthPixel
  * @description provided px is in 360 px height resolution,
  *    and this function will convert to the value relatively to viewPort width
  */
- function widthPixcel(px: number) {
+ function widthPixel(px: number) {
   const ratio = px / 360;
   return Math.round(ratio * viewportWidth);
 }
 
+function widthPercent(percentage: number) {
+  const value = (percentage * viewportWidth) / 100;
+  return Math.round(value);
+}
+
 export {
-  widthPixcel as wpx,
+  widthPercent as wp,
+  widthPixel as wpx,
 };
