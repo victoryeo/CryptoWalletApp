c37d9f53f8f3af44f5c440c41eef4b35ef458a30
victoryeo <victoryeo1974@gmail.com>
2022-02-24 11:31:50 +0800

    add modal reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import Wallet from './wallet';
+import Modal from './modal';
 
 // Actions
 export const actions = {
   wallet: Wallet.actions,
+  modal: Modal.actions,
 };
 
 // Reducers
 export default combineReducers({
   wallet: Wallet.reducer,
+  modal: Modal.reducer,
 });
