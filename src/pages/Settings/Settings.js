4b1ac767c36ddce667e4705201e144d159d412a4
victoryeo <victoryeo1974@gmail.com>
2022-02-24 15:31:13 +0800

    add settings page

---
+++ src/pages/Settings/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SafeAreaView from 'src/components/SafeAreaView';
+import styles from './Settings.css';
+
+const Settings = ({ navigation }) => {
+  return (
+    <SafeAreaView style={[styles.bgContainer]}>
+
+    </SafeAreaView>
+  )
+}
+
+export default Settings;
