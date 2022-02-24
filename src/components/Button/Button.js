e5f0e07457e0c78e9c63601a9e5fd1a5e0220c91
yeosiowvic <yeosiowvic@gmail.com>
2022-02-24 10:56:29 +0800

    add customised Button and TextInput

---
+++ src/components/Button/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+export default function Button({ label, onPress }) {
+  return (
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        borderRadius: 8,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1148fe'
+      }}
+      activeOpacity={0.7}
+      onPress={onPress}
+    >
+      <Text
+        style={{ fontSize: 18, color: 'white', textTransform: 'uppercase' }}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+}
