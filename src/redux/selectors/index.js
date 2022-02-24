7a8ae36ad89991a0fdf008624fb9a4bdcd4d2c09
victoryeo <victoryeo1974@gmail.com>
2022-02-24 13:17:48 +0800

    add selectors

---
+++ src/redux/selectors/index.js
@@ -0,0 +1,5 @@
+import * as walletSelectors from './wallet';
+
+export default {
+  ...walletSelectors,
+}
---
+++ src/redux/selectors/wallet.js
@@ -0,0 +1,2 @@
+export const password = state => state.wallet.password;
+export const seedPhrase = state => state.wallet.seedPhrases;
