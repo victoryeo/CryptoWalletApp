b299cf27d27217c136b2f3f4de695ee55c9387f6
yeosiowvic <yeosiowvic@gmail.com>
2022-02-24 21:24:33 +0800

    update Tab navigator

--- src/routes/stacks/MainTabs.js
+++ src/routes/stacks/MainTabs.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Wallet from 'src/pages/Wallet';
 import Settings from 'src/pages/Settings';
 import { wpx } from 'utils/dimensions';
@@ -14,43 +14,34 @@ const options = {
   gestureEnabled: false,
 };
 
-const tabParams = {
-  Wallet: {
-    renderIcon: (color) => (
-      <MaterialCommunityIcons name="wallet" size={wpx(22)} color={color} />
-    ),
-  },
-  Settings: {
-    renderIcon: (color) => (
-      <MaterialCommunityIcons name="briefcase" size={wpx(20)} color={color} />
-    ),
-  },
-};
-
 const MainTabs = () => (
-  <View>
+  <View style={g.custom.flex1}>
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color }) => {
-          const { renderIcon } = tabParams[route.name] || {};
-          return renderIcon ? renderIcon(color) : null;
+      screenOptions={({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Wallet') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused 
+              ? 'cloud-upload' 
+              : 'cloud-upload-outline';
+          }
+          console.log(iconName)
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: g.colors.lightPurple2,
-        tabBarInactiveTintColor: g.colors.darkPurple,
-        tabBarStyle: {
-          backgroundColor: g.colors.darkPurple2,
-          borderTopWidth: 0,
-          height: wpx(60),
-        },
-        tabBarIconStyle: {
-          marginTop: wpx(6),
-        },
-        tabBarLabelStyle: {
-          fontSize: 10,
-          paddingBottom: wpx(6),
+        tabBarActiveTintColor: '#e91e63',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: { position: 'absolute' },
+          tabBarLabelStyle: {
+          fontSize: 14,
+          paddingBottom: wpx(2),
         },
       })}
-      sceneContainerStyle={{ backgroundColor: 'transparent' }}
       initialRouteName="Wallet"
     >
       <Tab.Screen name="Wallet" component={Wallet} options={options} />
