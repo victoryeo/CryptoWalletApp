71cca5081a8ff178fb4e1d223f4388eefcb2fef1
victoryeo <victoryeo1974@gmail.com>
2022-02-24 16:06:42 +0800

    add MainTabs to route stack

--- src/routes/RoutesStack.js
+++ src/routes/RoutesStack.js
@@ -1,19 +1,37 @@
 import * as React from 'react';
 import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useSelector } from 'react-redux';
 
 import { initNavigator } from '../utils/NavigationService';
 import Launch from '../pages/launch';
-import { StartScreens } from './stacks';
+import { MainTabs, StartScreens } from './stacks';
 
 const Stack = createNativeStackNavigator();
 
 const RoutesStack = () => {
+  const { isAppInitialized, isAuthenticated } = useSelector(state => state.auth);
 
   const renderScreens = () => {
+    if (!isAppInitialized) {
+      //return <Stack.Screen name="Launch" component={Launch} />;
+    }
 
-    //return <Stack.Screen name="Launch" component={Launch} />;
-    return <Stack.Screen name="StartScreens" component={StartScreens} />;
+    if (!isAuthenticated) {
+      return <Stack.Screen name="StartScreens" component={StartScreens} />;
+    }
+
+    return (
+      <>
+        <Stack.Screen
+          name="Main"
+          component={MainTabs}
+          options={{
+            animationTypeForReplace: 'push',
+          }}
+        />
+      </>
+    );
   };
 
   return (
