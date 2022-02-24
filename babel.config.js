7277d57f8c313da895ffb964b5d108933f8da1f1
yeosiowvic <yeosiowvic@gmail.com>
2022-02-24 09:57:27 +0800

    update babel for using root path in RN

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        paths: [
+          {
+            rootPathSuffix: './src',
+            rootPathPrefix: 'src',
+          },
+          {
+            rootPathSuffix: './src/components',
+            rootPathPrefix: 'components',
+          },
+          {
+            rootPathSuffix: './src/utils',
+            rootPathPrefix: 'utils',
+          },
+          {
+            rootPathSuffix: './src/assets',
+            rootPathPrefix: 'assets',
+          },
+          {
+            rootPathSuffix: './src/pages',
+            rootPathPrefix: 'pages',
+          },
+          {
+            rootPathSuffix: './src/redux',
+            rootPathPrefix: '@crypto-redux',
+          },
+        ],
+      },
+    ],
+  ],
 };
