cdb25e238a9385fc3c9a18b91d6231176bb2fea7
yeosiowvic <yeosiowvic@gmail.com>
2022-02-24 15:31:21 +0800

    add wallet page

---
+++ src/pages/Wallet/Wallet.css.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+import { wpx , wp } from 'utils/dimensions';
+import g from 'src/assets/globalStyle';
+
+const MARGIN_HOR = wpx (16);
+
+export default StyleSheet.create({
+  bgContainer: {
+    flex: 1,
+  },
+  creditCard: {
+    marginVertical: wpx(6),
+    marginHorizontal: MARGIN_HOR,
+    width: wp(100) - MARGIN_HOR * 2,
+  },
+  sectionBalance: {
+    marginHorizontal: g.constant.SIDE_MARGIN,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  accountNumber: {
+    marginHorizontal: g.constant.SIDE_MARGIN,
+  }
+});
---
+++ src/pages/Wallet/Wallet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SafeAreaView from 'src/components/SafeAreaView';
+import styles from './Wallet.css.js';
+
+const Wallet = ({ navigation }) => {
+  return (
+    <SafeAreaView style={[styles.bgContainer]}>
+
+    </SafeAreaView>
+  )
+}
+
+export default Wallet;
