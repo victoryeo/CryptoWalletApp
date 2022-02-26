f713fe817297170119d8c8915dc27c811bb5f3b1
yeosiowvic <victoryeo1974@gmail.com>
2022-02-26 13:17:39 +0800

    add ImportSeed stack screen

--- src/routes/stacks/StartScreens.js
+++ src/routes/stacks/StartScreens.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Onboarding from '../../pages/onboarding';
-import Landing from '../../pages/onboarding/Landing';
-import CreateWallet from '../../pages/onboarding/CreateWallet';
+import Onboarding from 'src/pages/onboarding';
+import Landing from 'src/pages/onboarding/Landing';
+import CreateWallet from 'src/pages/onboarding/CreateWallet';
+import ImportSeed from 'src/pages/onboarding/ImportSeed';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,7 @@ const StartScreens = () => (
     <Stack.Screen name="Onboarding" component={Onboarding} />
     <Stack.Screen name="Landing" component={Landing} options={customOptions} />
     <Stack.Screen name="CreateWallet" component={CreateWallet} />
+    <Stack.Screen name="ImportSeed" component={ImportSeed} />
   </Stack.Navigator>
 );
 
