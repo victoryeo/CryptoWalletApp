54bd8f950204769cf95799e84d891aca37d01926
yeosiowvic <victoryeo1974@gmail.com>
2022-02-27 21:52:38 +0800

    combine two files into Web3ClientFunc.js

--- src/utils/Web3ClientFunc.js
+++ src/utils/Web3ClientFunc.js
@@ -1,4 +1,12 @@
-import { web3Client } from 'src/utils/initWeb3Client';
+import Web3Client from './Web3Client';
+
+export let web3Client = null;
+
+export const initWeb3Client = () => {
+  if (!web3Client) {
+    web3Client = new Web3Client();
+  }
+}
 
 export const getAccountBalance = async (address) => {
   const ret = await web3Client.getAccountBalance(address);
--- src/utils/initWeb3Client.js
+++
@@ -1,9 +0,0 @@
-import Web3Client from './Web3Client';
-
-export let web3Client = null;
-
-export const initWeb3Client = () => {
-  if (!web3Client) {
-    web3Client = new Web3Client();
-  }
-}
