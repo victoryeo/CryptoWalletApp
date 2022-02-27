e2363b012137687e08af063d108481d0ca3a87b4
yeosiowvic <victoryeo1974@gmail.com>
2022-02-27 22:13:09 +0800

    minor update

--- src/redux/sagas/handlers/general.js
+++ src/redux/sagas/handlers/general.js
@@ -33,13 +33,13 @@ function* initApp() {
 }
 
 function* rehydrate() {
-  console.log('-- APP INIT --');
+  console.log('--- APP INIT ---');
 
   // We want to launch screen to show at least 100msec for the animation
   yield all([call(initApp), delay(100)]);
 
   yield put(authActions.APP_INIT_DONE());
-  console.log('-- APP INIT DONE --');
+  console.log('--- APP INIT DONE ---');
 }
 
 export default function* generalSaga() {
