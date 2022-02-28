6aaedf29892891f5e3e0f38febf2fc6386366877
yeosiowvic <victoryeo1974@gmail.com>
2022-02-28 21:17:46 +0800

    add Header to Send

--- src/pages/Send/Send.js
+++ src/pages/Send/Send.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import SafeAreaView from 'src/components/SafeAreaView';
+import Header from 'src/components/Header';
 import styles from './Send.css';
+import globalS from 'src/assets/globalStyle';
+import { pop } from 'utils/NavigationService';
 
 const Send = ({ navigation }) => {
 
   return (
     <SafeAreaView style={[styles.container]}>
-      <Text style={styles.bigwords}>Send</Text>
+      <Header
+        title="Send"
+        leftIcon={{ name: globalS.icon.BACK, onPress: pop }}
+      />
+      <Text style={styles.words}>to</Text>
     </SafeAreaView>)
 }
 
