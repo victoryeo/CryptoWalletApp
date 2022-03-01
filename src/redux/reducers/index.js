12b9c2c479c74087d0cf38df05a437785d9f98be
yeosiowvic <victoryeo1974@gmail.com>
2022-03-01 21:31:14 +0800

    remove modal action

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,18 +1,15 @@
 import { combineReducers } from 'redux';
 import Wallet from './wallet';
-import Modal from './modal';
 import Auth from './auth';
 
 // Actions
 export const actions = {
   wallet: Wallet.actions,
-  modal: Modal.actions,
   auth: Auth.actions,
 };
 
 // Reducers
 export default combineReducers({
   wallet: Wallet.reducer,
-  modal: Modal.reducer,
   auth: Auth.reducer,
 });
