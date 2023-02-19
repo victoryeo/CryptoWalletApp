59e2b893602da62cc47541113d07fb064033254a
yeosiowvic <victoryeo1974@gmail.com>
2023-02-19 10:57:41 +0800

    use @react-native-async-storage because @react-native-community/async-storage is deprecated

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -10,7 +10,7 @@ import {
 } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import reducer from './reducers';
 import rootSaga from './sagas/rootSaga';
